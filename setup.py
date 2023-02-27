cfc1fa69165e7c26e701344e7fe18d512bef5b29
Shideh Almasian <almasian@stud.uni-heidelberg.de>
2023-02-27 13:37:08 +0100

    added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CQE',
+    version='1.0.0',
+    packages=['rule_set', 'evaluation', 'evaluation.gpt-3'],
+    url='',
+    license='',
+    author='satyaalmasian',
+    author_email='satya.almasian@gmail.com',
+    description='CQE: quantity extractor'
+)
